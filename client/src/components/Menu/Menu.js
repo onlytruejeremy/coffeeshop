6311ffb57a386bcb0bdcc7ca5dd21e1d387c385d
onlytruejeremy <jeremydev2020@gmail.com>
2021-02-06 17:38:48 -0800

    Added modeldata

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -9,10 +9,48 @@ const Menu = (props) => {
     "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
     "https://images.unsplash.com/photo-1610735458851-bf3be7078588?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
   ];
-
-  const [showModal, setShowModal] = React.useState(true);
+  const [modalContent, setModalContent] = React.useState([]);
+  const [showModal, setShowModal] = React.useState(false);
   const triggerModal = () => {
     setShowModal(!showModal);
+  };
+  const modalData = [
+    {
+      id: 1,
+      category: "donuts",
+      items: [
+        { name: "glazed", price: 1 },
+        { name: "sprinkles", price: 1.5 },
+        { name: "boston creme", price: 2 },
+      ],
+    },
+    {
+      id: 2,
+      category: "bagels",
+      items: [
+        { name: "plain", price: 1 },
+        { name: "blueberry", price: 1.5 },
+        { name: "strawberry", price: 1.5 },
+      ],
+    },
+    {
+      id: 3,
+      category: "coffee",
+      items: [
+        { name: "black", price: 2 },
+        { name: "mocha", price: 3 },
+        { name: "frappe", price: 3 },
+      ],
+    },
+  ];
+  const prepareModal = (e) => {
+    e.preventDefault();
+    let name = e.target.name;
+    let data = modalData.filter((item) => {
+      return item.category === name;
+    });
+    setModalContent(data);
+    triggerModal();
   };
   return (
     <>
@@ -29,7 +67,13 @@ const Menu = (props) => {
                   We've got all the caffeine. Iced, Hot, Espresso, whatever you
                   are in the mood for!
                 </p>
-                <button className="btn btn__primary">View Coffee</button>
+                <button
+                  className="btn btn__primary"
+                  name="coffee"
+                  onClick={prepareModal}
+                >
+                  View Coffee
+                </button>
               </div>
             </div>
             <div
@@ -42,7 +86,13 @@ const Menu = (props) => {
                   Sprinkled, Glazed, Filled, it's all there. We're sure there's
                   something you'll like.
                 </p>
-                <button className="btn btn__primary">View Donuts</button>
+                <button
+                  className="btn btn__primary"
+                  name="donuts"
+                  onClick={prepareModal}
+                >
+                  View Donuts
+                </button>
               </div>
             </div>
             <div
@@ -55,12 +105,18 @@ const Menu = (props) => {
                   Sometimes all you need is a bagel and cream cheese. Don't
                   worry, we've got different kinds if plain simply won't do.
                 </p>
-                <button className="btn btn__primary">View Bagels</button>
+                <button
+                  className="btn btn__primary"
+                  name="bagels"
+                  onClick={prepareModal}
+                >
+                  View Bagels
+                </button>
               </div>
             </div>
           </div>
         </div>
-        {showModal ? <Modal set={triggerModal} content={null} /> : null}
+        {showModal ? <Modal set={triggerModal} content={modalContent} /> : null}
       </Layout>
     </>
   );
--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -5,17 +5,40 @@ import "./Modal.css";
 const Modal = (props) => {
   return (
     <div className={`modal__container active`}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, odit?
-      </p>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          props.set();
-        }}
-      >
-        Close Modal
-      </button>
+      <div className="modal__inner">
+        <table>
+          <thead>{props.content[0].category.toUpperCase()}</thead>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+            {props.content[0].items.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.name.toUpperCase()}</td>
+                  <td>${item.price}</td>
+                  <td className="td__flex">
+                    <span>&#8593;</span>
+                    <input type="text" />
+                    <span> &#8595;</span>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <button
+          className="btn btn__primary"
+          onClick={(e) => {
+            e.preventDefault();
+            props.set();
+          }}
+        >
+          Close
+        </button>
+      </div>
     </div>
   );
 };
